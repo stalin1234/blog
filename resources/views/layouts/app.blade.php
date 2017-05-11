223b67159d254ca7a430f0aa87a90632cfedee6d
Frank Sancho <afsc.analysis@gmail.com>
2017-05-11 14:57:54 -0300

    Iniciando proyecto blog

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+
+    <meta charset="UTF-8">
+    <title>Reddit Clone</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+
+
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-12">
+
+               <h1>Reditt Clone ooohhhhhh</h1>
+                
+            </div>
+        
+        </div>
+
+        <hr>
+
+       
+       @yield('content')
+
+
+
+    </div>
+
+
+</body>
+</html>
